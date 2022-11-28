168c5a5f15a45d82f17b478ac1e59206ea3e25ea
Dev2a <contact@dev2a.pro>
2022-11-28 11:44:41 +0100

    [Feat] add compilation for cjs

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+const plugins = [
+    babel({exclude: 'node_modules/realm', babelHelpers: "bundled"}),
+    resolve({
+        // module: true,
+        // jsnext: true,
+        // main: true,
+        preferBuiltins: true,
+        browser: false,
+        modulesOnly: true,
+    }),
+    commonjs({
+        ignore: 'core-js'
+    }),
+];
+
+const createConfig = (filename) => ({
+    input: `./${filename}.js`,
+    output: [
+        {
+            file: `./dist/${filename}.js`,
+            format: 'cjs',
+            exports: 'auto',
+        }
+    ],
+    external: ['realm'],
+    plugins,
+});
+
+
+const configs = [
+    'index',
+    'Model',
+].map((filename) => createConfig(filename));
+
+export default configs;
